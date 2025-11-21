d4b4dc59f12f12ac379d13856c1c4a22fbeb9029
deeznustjnc <160991172+deeznustjnc@users.noreply.github.com>
2025-11-21 21:54:54 +0000

    Fix Scramjet config and worker

--- scram/scramjet.config.js
+++ scram/scramjet.config.js
@@ -1,1 +1,10 @@
-let _CONFIG = {};
+self.$scramjet = {
+    config: {
+        prefix: "/service/",
+        codec: "plain",
+        wasm: "/scram/b806f43d79bff8f4.wasm",
+        worker: "/scram/scramjet.worker.js",
+        client: "/scram/scramjet.bundle.js",
+        sync: "/scram/scramjet.sync.js"
+    }
+};
